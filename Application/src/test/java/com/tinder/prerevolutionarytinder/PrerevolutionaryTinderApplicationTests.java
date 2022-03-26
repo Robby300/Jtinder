b9af4ed22786b039921fe9b713a68e5f9c7fc832
robert <robby300im@gmail.com>
2022-03-26 20:47:25 +0500

    Initial commit

---
+++ Application/src/test/java/com/tinder/prerevolutionarytinder/PrerevolutionaryTinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tinder.prerevolutionarytinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrerevolutionaryTinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
