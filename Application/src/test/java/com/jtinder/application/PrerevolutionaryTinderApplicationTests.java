461852bf7c6bd1721e232ef6b23aa7d3548e6dd7
robert <robby300im@gmail.com>
2022-03-27 01:29:59 +0500

    initialize

--- Application/src/test/java/com/jtinder/application/PrerevolutionaryTinderApplicationTests.java
+++ Application/src/test/java/com/jtinder/application/PrerevolutionaryTinderApplicationTests.java
@@ -1,4 +1,4 @@
-package com.tinder.client;
+package com.jtinder.application;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
---
+++ Client/src/test/java/com/tinder/client/ClientTests.java
@@ -0,0 +1,13 @@
+package com.tinder.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
