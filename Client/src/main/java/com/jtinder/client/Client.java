f7d11228f7106082b29c426823807e8ba525b16a
robert <robby300im@gmail.com>
2022-04-07 12:05:29 +0500

    remove commented code

--- Client/src/main/java/com/jtinder/client/Client.java
+++ Client/src/main/java/com/jtinder/client/Client.java
@@ -5,25 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Client {
-//    static final String URL_USERS_JSON = "http://localhost:8080/users";
-//    static final String URL_CREATE_USER = "http://localhost:8080/users";
-//
-//    RestTemplate restTemplate = new RestTemplate();
-//
-//    // Send request with GET method and default Headers.
-//    String result = restTemplate.getForObject(URL_USERS_JSON, String.class);
-//
-//
-//    User userForPost = User.builder()
-//            .name("Васян")
-//            .description("Я парень неплохой, только ссуся и глухой")
-//            .findSex(Sex.FEMALE)
-//            .build();
-//
-//    // Send request with POST method.
-//    User u = restTemplate.postForObject(URL_CREATE_USER, userForPost, User.class);
-//    int i = 0;
-
 
     public static void main(String[] args) {
         SpringApplication.run(Client.class, args);
