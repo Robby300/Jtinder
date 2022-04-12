5c6ce03a8cbe4ba657db73771aed42f1d492a69a
robert <robby300im@gmail.com>
2022-04-12 13:06:28 +0500

    fix issues

--- Client/src/main/java/com/jtinder/client/telegram/service/ServerService.java
+++ Client/src/main/java/com/jtinder/client/telegram/service/ServerService.java
@@ -29,19 +29,19 @@ public class ServerService {
     private final PrerevolutionaryTranslator translator;
 
     @Value("${key.localPath}")
-    static private String localPath;
+    private String localPath;
 
-    static final String URL_USERS = localPath + "users/search";
-    static final String URL_LIKE = localPath + "users/like/%d";
-    static final String URL_UNLIKE = localPath + "users/unlike/%d";
-    static final String URL_REGISTRATION = localPath + "registration";
-    static final String URL_LOGIN = localPath + "login";
-    static final String URL_HOME = localPath + "home";
-    static final String URL_IS_REGISTERED = localPath + "users/exists/%d";
-    static final String URL_LOWERS = localPath + "users/likers";
-    static final String URL_CAPTION = localPath + "users/imgdescr/%d";
-    static final String URL_LOVE = localPath + "users/islove/%d";
-    static final String URL_CHANGE_CURRENT = localPath + "users/update_current";
+    static final String URL_USERS = "users/search";
+    static final String URL_LIKE = "users/like/%d";
+    static final String URL_UNLIKE = "users/unlike/%d";
+    static final String URL_REGISTRATION = "registration";
+    static final String URL_LOGIN = "login";
+    static final String URL_HOME = "home";
+    static final String URL_IS_REGISTERED = "users/exists/%d";
+    static final String URL_LOWERS = "users/likers";
+    static final String URL_CAPTION = "users/imgdescr/%d";
+    static final String URL_LOVE = "users/islove/%d";
+    static final String URL_CHANGE_CURRENT = "users/update_current";
 
     public ServerService(RestTemplate restTemplate, AuthorizationService authorizationService, PrerevolutionaryTranslator translator) {
         this.restTemplate = restTemplate;
@@ -51,7 +51,7 @@ public class ServerService {
 
 
     public List<Profile> getValidProfilesToUser(User user) {
-        ResponseEntity<Profile[]> usersResponse = restTemplate.exchange(URL_USERS, HttpMethod.GET, authorizationService.getAuthorizationHeader(user), Profile[].class);
+        ResponseEntity<Profile[]> usersResponse = restTemplate.exchange(localPath + URL_USERS, HttpMethod.GET, authorizationService.getAuthorizationHeader(user), Profile[].class);
         log.info("Получение {} анкет в поиске для текущего пользователя", usersResponse.getBody().length);
         return List.of(usersResponse.getBody());
     }
@@ -60,28 +60,29 @@ public class ServerService {
         HttpEntity<Void> authEntity = authorizationService.getAuthorizationHeader(user);
         HttpEntity<Profile> entity = new HttpEntity<>(profile, authEntity.getHeaders());
         log.info("Обновление текущего пользователя с id = {}", profile.getUserId());
-        restTemplate.postForObject(URL_CHANGE_CURRENT, entity, Void.class);
+        restTemplate.postForObject(localPath + URL_CHANGE_CURRENT, entity, Void.class);
     }
 
     public void unLikeProfile(Long profileId, User user) {
-        restTemplate.put(String.format(URL_UNLIKE, profileId), authorizationService.getAuthorizationHeader(user), profileId, Long.class);
+        restTemplate.put(String.format(localPath + URL_UNLIKE, profileId), authorizationService.getAuthorizationHeader(user), profileId, Long.class);
         log.info("Текущий пользователь id = {} ставит лайк пользоваетлю id ={}", user.getProfile().getUserId(), profileId);
     }
 
     public boolean isRegistered(Long userId) {
         log.info("Регистрации пользователя с id = {}", userId);
-        return restTemplate.getForObject(String.format(URL_IS_REGISTERED, userId), Boolean.class);
+        String test = localPath;
+        return restTemplate.getForObject(String.format(localPath + URL_IS_REGISTERED, userId), Boolean.class);
     }
 
     public void registerUser(Profile profile) {
         log.info("Регистрации пользователя с id = {}", profile.getUserId());
-        restTemplate.postForObject(URL_REGISTRATION, profile, Profile.class);
+        restTemplate.postForObject(localPath + URL_REGISTRATION, profile, Profile.class);
     }
 
     public String loginUser(AuthenticUser authenticUser) {
         HttpEntity<AuthenticUser> entity = new HttpEntity<>(authenticUser);
         try {
-            Token token = restTemplate.postForObject(URL_LOGIN, entity, Token.class);
+            Token token = restTemplate.postForObject(localPath + URL_LOGIN, entity, Token.class);
             log.info("Авторизация пользователя = {}", authenticUser.getUsername());
             return token.getToken();
         } catch (HttpClientErrorException e) {
@@ -92,28 +93,28 @@ public class ServerService {
 
     public Profile getLoginUserProfile(User user) {
         log.info("Запрос текущего пользователя id = {}", user.getProfile().getUserId());
-        return restTemplate.postForObject(URL_HOME, authorizationService.getAuthorizationHeader(user), Profile.class);
+        return restTemplate.postForObject(localPath + URL_HOME, authorizationService.getAuthorizationHeader(user), Profile.class);
     }
 
     public void likeProfile(Long profileId, User user) {
-        restTemplate.put(String.format(URL_LIKE, profileId), authorizationService.getAuthorizationHeader(user), profileId, Long.class);
+        restTemplate.put(String.format(localPath + URL_LIKE, profileId), authorizationService.getAuthorizationHeader(user), profileId, Long.class);
         log.info("Текущий пользователь id = {} ставит лайк пользоваетлю id ={}", user.getProfile().getUserId(), profileId);
     }
 
     public List<Profile> getLowersProfilesToUser(User user) {
-        ResponseEntity<Profile[]> usersResponse = restTemplate.exchange(URL_LOWERS, HttpMethod.GET, authorizationService.getAuthorizationHeader(user), Profile[].class);
+        ResponseEntity<Profile[]> usersResponse = restTemplate.exchange(localPath + URL_LOWERS, HttpMethod.GET, authorizationService.getAuthorizationHeader(user), Profile[].class);
         log.info("Получение всех анкет, у которых есть отношения с текущим пользователем id = {} в количестве = {}", user.getProfile().getUserId(), usersResponse.getBody().length);
         return List.of(usersResponse.getBody());
     }
 
     public String getCaption(Long userId, User user) {
-        ResponseEntity<String> caption = restTemplate.exchange(String.format(URL_CAPTION, userId), HttpMethod.GET, authorizationService.getAuthorizationHeader(user), String.class);
+        ResponseEntity<String> caption = restTemplate.exchange(String.format(localPath + URL_CAPTION, userId), HttpMethod.GET, authorizationService.getAuthorizationHeader(user), String.class);
         log.info("Получение описания к картинке профиля для пользователя id = {}", userId);
         return translator.translate(caption.getBody());
     }
 
     public boolean weLove(Long userId, User user) {
-        ResponseEntity<Boolean> love = restTemplate.exchange(String.format(URL_LOVE, userId), HttpMethod.GET, authorizationService.getAuthorizationHeader(user), boolean.class);
+        ResponseEntity<Boolean> love = restTemplate.exchange(String.format(localPath + URL_LOVE, userId), HttpMethod.GET, authorizationService.getAuthorizationHeader(user), boolean.class);
         log.info("Получение popUp сообщения для пользователя id = {}", userId);
         return love.getBody();
     }
